535d54fbdde650a76d765e98ac5894498416f894
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-08-14 13:21:02 +0530

    env2

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,3 @@
-// module.exports = {
-// 	MONGO_URI:
-// 		'mongodb+srv://mrudul:1203@cluster0-2nil5.mongodb.net/test?retryWrites=true&w=majority',
-// 	JWT_SECRET_KEY: 'examapp',
-// };
-
-//'mongodb+srv://mrudul:1203@cluster0-2nil5.mongodb.net/test?retryWrites=true&w=majority'
-
 module.exports = {
 	MONGO_URI: process.env.MONGO_URI,
 	JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
